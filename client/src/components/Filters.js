83c99c89026d2b5234b772feeb4a340db3f22c1d
IvanIglinsky <119114301+IvanIglinsky@users.noreply.github.com>
2023-06-15 12:47:32 +0300

    Clock Store

---
+++ client/src/components/Filters.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SortList from "./SortList";
+
+const Filters = () => {
+    return (
+        <div>
+        <h3 className="ml-2">
+        </h3>
+            <SortList/>
+        </div>
+    );
+};
+
+export default Filters;
